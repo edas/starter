830f55098720f6c0ffd7ba08d7e38de74a36f620
Éric D <15271+edas@users.noreply.github.com>
2019-09-27 12:04:18 +0200

    do not download if already downloaded

--- remote-exec.sh
+++ remote-exec.sh
@@ -2,7 +2,10 @@
 
 # curl -fsSL https://raw.githubusercontent.com/edas/starter/master/remote-exec.sh | /usr/bin/env bash
 
-curl https://github.com/edas/starter/archive/master.zip -L -o starter-master.zip
-unzip starter-master.zip
-pushd starter-master
-./link && ./install && ./starter && popd && rm -rf starter-master*
+if [ ! -e "./starter-master" ]; then
+  if [ ! -e "./starter-master.zip" ]; then
+    curl https://github.com/edas/starter/archive/master.zip -L -o starter-master.zip
+  fi
+  unzip starter-master.zip
+fi  
+pushd starter-master && ./link && ./install && ./starter && popd && rm -rf starter-master*
