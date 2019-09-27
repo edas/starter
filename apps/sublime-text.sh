3c10850846840a2a301c33ebd309f5e9a3402c56
Éric D <15271+edas@users.noreply.github.com>
2019-09-27 13:21:05 +0200

    create sublime text path before changing things

--- apps/sublime-text.sh
+++ apps/sublime-text.sh
@@ -7,9 +7,12 @@
 LOCAL_PATH='apps/sublime'
 
 # Install Package Control
-cd ~/Library/Application\ Support/Sublime\ Text\ 3/Installed\ Packages && { curl -sLO https://packagecontrol.io/Package\ Control.sublime-package ; cd -; }
+[ ! -e ~/Library/Application\ Support/Sublime\ Text\ 3/Installed\ Packages ] mkdir -p ~/Library/Application\ Support/Sublime\ Text\ 3/Installed\ Packages
+pushd ~/Library/Application\ Support/Sublime\ Text\ 3/Installed\ Packages && { curl -sLO https://packagecontrol.io/Package\ Control.sublime-package ; }
+popd
 
 # Install Sublime Text settings
+[ ! -e ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User ] mkdir -p ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User
 cp -r ${LOCAL_PATH}/* ~/Library/Application\ Support/Sublime\ Text*/Packages/User/ 2>/dev/null
 
 # Set Sublime Text as default plain text editor
