04321cbca3f24e02d47989cb8a78be5b8f9185d7
Éric D <15271+edas@users.noreply.github.com>
2019-10-01 12:36:45 +0200

    Update sharing.sh

--- system/sharing.sh
+++ system/sharing.sh
@@ -9,5 +9,5 @@
 [ -n "$COMPUTER_NAME" ] && sudo scutil --set ComputerName "$COMPUTER_NAME"
 [ -n "$HOST_NAME" ] && sudo scutil --set HostName "$HOST_NAME"
 [ -n "$HOST_NAME" ] && sudo scutil --set LocalHostName "$HOST_NAME"
-[ -n "$HOST_NAME" ] && sudo defaults write /Library/Preferences/SystemConfiguration/com.apple.smb.server \
+[ -n "$HOST_NAME" ] && sudo defaults write /Library/Preferences/SystemConfiguration/com.apple.smb.server.plist \
     NetBIOSName -string "$HOST_NAME"
